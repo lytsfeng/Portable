d51b69ddfca37c9ffb0ebd7d51f6dbc9a8f548d8
john <lytsfeng@gmail.com>
2015-08-12 16:35:36 +0800

    去掉公司名称

--- app/build/generated/source/r/debug/com/ldkj/portable/R.java
+++ app/build/generated/source/r/debug/com/ldkj/portable/R.java
@@ -9,13 +9,13 @@ package com.ldkj.portable;
 
 public final class R {
     public static final class array {
-        public static final int array_attenuation_control=0x7f040000;
-        public static final int array_demodulation_mode=0x7f040001;
-        public static final int array_filter_bandwidth=0x7f040002;
-        public static final int array_freq_bandwidth=0x7f040003;
-        public static final int array_sound=0x7f040004;
-        public static final int array_squelch_threshold=0x7f040005;
-        public static final int levelavg=0x7f040006;
+        public static final int array_attenuation_control=0x7f060000;
+        public static final int array_demodulation_mode=0x7f060001;
+        public static final int array_filter_bandwidth=0x7f060002;
+        public static final int array_freq_bandwidth=0x7f060003;
+        public static final int array_sound=0x7f060004;
+        public static final int array_squelch_threshold=0x7f060005;
+        public static final int levelavg=0x7f060006;
     }
     public static final class attr {
         /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
@@ -45,10 +45,10 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int type=0x7f010002;
     }
     public static final class color {
-        public static final int black=0x7f080000;
-        public static final int color_ff6f84a3=0x7f080001;
-        public static final int transparent=0x7f080002;
-        public static final int white=0x7f080003;
+        public static final int black=0x7f040000;
+        public static final int color_ff6f84a3=0x7f040001;
+        public static final int transparent=0x7f040002;
+        public static final int white=0x7f040003;
     }
     public static final class dimen {
         public static final int activity_horizontal_margin=0x7f050000;
@@ -188,75 +188,75 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int menu_main=0x7f090000;
     }
     public static final class string {
-        public static final int ButtonTextClose=0x7f060000;
-        public static final int ButtonTextDot=0x7f060001;
-        public static final int ButtonTextDoubleZero=0x7f060002;
-        public static final int ButtonTextEight=0x7f060003;
-        public static final int ButtonTextFive=0x7f060004;
-        public static final int ButtonTextFour=0x7f060005;
-        public static final int ButtonTextGHZ=0x7f060006;
-        public static final int ButtonTextKHZ=0x7f060007;
-        public static final int ButtonTextMHZ=0x7f060008;
-        public static final int ButtonTextNine=0x7f060009;
-        public static final int ButtonTextNo=0x7f06000a;
-        public static final int ButtonTextOne=0x7f06000b;
-        public static final int ButtonTextSeven=0x7f06000c;
-        public static final int ButtonTextSix=0x7f06000d;
-        public static final int ButtonTextThree=0x7f06000e;
-        public static final int ButtonTextTwo=0x7f06000f;
-        public static final int ButtonTextYes=0x7f060010;
-        public static final int ButtonTextZero=0x7f060011;
-        public static final int ButtonTextback=0x7f060012;
-        public static final int about=0x7f060013;
-        public static final int action_settings=0x7f060014;
-        public static final int app_name=0x7f060015;
-        public static final int attenuation_control=0x7f060016;
-        public static final int cancel=0x7f060017;
-        public static final int center_freq=0x7f060018;
-        public static final int company=0x7f060019;
-        public static final int demodulation_mode=0x7f06001a;
-        public static final int device_conn_err=0x7f06001b;
-        public static final int device_conn_ok=0x7f06001c;
-        public static final int error=0x7f06001d;
-        public static final int filter_bandwidth=0x7f06001e;
-        public static final int freq_bandwidth=0x7f06001f;
-        public static final int hello_world=0x7f060020;
-        public static final int install=0x7f060021;
-        public static final int install_tip=0x7f060022;
-        public static final int level_avg=0x7f060023;
-        public static final int menu_setting=0x7f060024;
-        public static final int offlinemap=0x7f060025;
-        public static final int scan=0x7f060026;
-        public static final int setting=0x7f060027;
-        public static final int setting_server_ip=0x7f060028;
-        public static final int setting_server_tcp_port=0x7f060029;
-        public static final int setting_server_udp_port=0x7f06002a;
-        public static final int setting_tip=0x7f06002b;
-        public static final int single=0x7f06002c;
-        public static final int sound=0x7f06002d;
-        public static final int sound_title=0x7f06002e;
-        public static final int squelch_threshold=0x7f06002f;
-        public static final int string_cancel=0x7f060030;
-        public static final int string_ok=0x7f060031;
-        public static final int title_activity_about=0x7f060032;
-        public static final int title_activity_dialog=0x7f060033;
-        public static final int title_activity_freq_scan=0x7f060034;
-        public static final int title_activity_map=0x7f060035;
-        public static final int title_activity_param__dialog=0x7f060036;
-        public static final int title_activity_setting=0x7f060037;
-        public static final int title_activity_single_measure=0x7f060038;
+        public static final int ButtonTextClose=0x7f070000;
+        public static final int ButtonTextDot=0x7f070001;
+        public static final int ButtonTextDoubleZero=0x7f070002;
+        public static final int ButtonTextEight=0x7f070003;
+        public static final int ButtonTextFive=0x7f070004;
+        public static final int ButtonTextFour=0x7f070005;
+        public static final int ButtonTextGHZ=0x7f070006;
+        public static final int ButtonTextKHZ=0x7f070007;
+        public static final int ButtonTextMHZ=0x7f070008;
+        public static final int ButtonTextNine=0x7f070009;
+        public static final int ButtonTextNo=0x7f07000a;
+        public static final int ButtonTextOne=0x7f07000b;
+        public static final int ButtonTextSeven=0x7f07000c;
+        public static final int ButtonTextSix=0x7f07000d;
+        public static final int ButtonTextThree=0x7f07000e;
+        public static final int ButtonTextTwo=0x7f07000f;
+        public static final int ButtonTextYes=0x7f070010;
+        public static final int ButtonTextZero=0x7f070011;
+        public static final int ButtonTextback=0x7f070012;
+        public static final int about=0x7f070013;
+        public static final int action_settings=0x7f070014;
+        public static final int app_name=0x7f070015;
+        public static final int attenuation_control=0x7f070016;
+        public static final int cancel=0x7f070017;
+        public static final int center_freq=0x7f070018;
+        public static final int company=0x7f070019;
+        public static final int demodulation_mode=0x7f07001a;
+        public static final int device_conn_err=0x7f07001b;
+        public static final int device_conn_ok=0x7f07001c;
+        public static final int error=0x7f07001d;
+        public static final int filter_bandwidth=0x7f07001e;
+        public static final int freq_bandwidth=0x7f07001f;
+        public static final int hello_world=0x7f070020;
+        public static final int install=0x7f070021;
+        public static final int install_tip=0x7f070022;
+        public static final int level_avg=0x7f070023;
+        public static final int menu_setting=0x7f070024;
+        public static final int offlinemap=0x7f070025;
+        public static final int scan=0x7f070026;
+        public static final int setting=0x7f070027;
+        public static final int setting_server_ip=0x7f070028;
+        public static final int setting_server_tcp_port=0x7f070029;
+        public static final int setting_server_udp_port=0x7f07002a;
+        public static final int setting_tip=0x7f07002b;
+        public static final int single=0x7f07002c;
+        public static final int sound=0x7f07002d;
+        public static final int sound_title=0x7f07002e;
+        public static final int squelch_threshold=0x7f07002f;
+        public static final int string_cancel=0x7f070030;
+        public static final int string_ok=0x7f070031;
+        public static final int title_activity_about=0x7f070032;
+        public static final int title_activity_dialog=0x7f070033;
+        public static final int title_activity_freq_scan=0x7f070034;
+        public static final int title_activity_map=0x7f070035;
+        public static final int title_activity_param__dialog=0x7f070036;
+        public static final int title_activity_setting=0x7f070037;
+        public static final int title_activity_single_measure=0x7f070038;
     }
     public static final class style {
         /**  Customize your theme here. 
          */
-        public static final int AppTheme=0x7f070000;
-        public static final int GridButtonText=0x7f070001;
-        public static final int NumberButtonText=0x7f070002;
-        public static final int StyleListView=0x7f070003;
-        public static final int StyleListViewItem=0x7f070004;
-        public static final int StyleTable=0x7f070005;
-        public static final int StyleTableTitle=0x7f070006;
-        public static final int StyleTableValue=0x7f070007;
+        public static final int AppTheme=0x7f080000;
+        public static final int GridButtonText=0x7f080001;
+        public static final int NumberButtonText=0x7f080002;
+        public static final int StyleListView=0x7f080003;
+        public static final int StyleListViewItem=0x7f080004;
+        public static final int StyleTable=0x7f080005;
+        public static final int StyleTableTitle=0x7f080006;
+        public static final int StyleTableValue=0x7f080007;
     }
     public static final class styleable {
         /** Attributes that can be used with a CustomImageView.
